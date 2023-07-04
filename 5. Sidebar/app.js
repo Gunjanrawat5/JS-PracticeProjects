6114dc9919d2cf33fab217c2a5ec7602b4340cc9
Gunjan <gunjanrawat5@gmail.com>
2023-07-04 14:13:35 +0530

    4th and 5th Project

---
+++ 5. Sidebar/app.js
@@ -0,0 +1,18 @@
+
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click',()=>{
+    if(sidebar.classList.contains('show-sidebar')){
+        sidebar.classList.remove('show-sidebar');
+    }
+    else{
+        sidebar.classList.add('show-sidebar');
+    }
+    // other way sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click',()=>{
+    sidebar.classList.remove('show-sidebar');
+})
